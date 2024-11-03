f3004d95ccdb2e463baf7dd9e027001248b96657
Yehuda Greenberg <yehuda@noemailspamplease.com>
2024-11-03 22:08:46 +0200

    added ports to motors

--- brick.py
+++ brick.py
@@ -1,2 +1,16 @@
-from pybricks.robotics import Robotics
+from pybricks.robotics import GyroDriveBase
+from pybricks.hubs import InventorHub
+from pybricks.pupdevices import Motor,ColorSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.tools import wait, StopWatch
 
+
+hub = InventorHub()
+
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.B)
+c_motor = Motor(Port.C)
+d_motor = Motor(Port.D)
+
+
+gyro_drive_base = GyroDriveBase(left_motor, right_motor, wheel_diameter=87)
